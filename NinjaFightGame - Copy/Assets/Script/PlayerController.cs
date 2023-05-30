b90741f4e81272b4f7ebaf9a0af5a4836a312787
colin bratton <colinbratton3@gmail.com>
2023-05-30 10:34:25 -0700

    started Worst case

---
+++ NinjaFightGame - Copy/Assets/Script/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    public float speed = 15.0f;
+    [SerializeField] bool isOnGround;
+    public float JumpForce = 15.0f;
+    public bool facingRight;
+    public GameObject projectileFire;
+    /*public GameObject projectileLightning;*/
+    public bool element;
+    private float angle = 180;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float forwardInput = Input.GetAxis("Vertical");
+    playerRB.AddForce(transform.forward * forwardInput * speed);
+    
+      if (Input.GetKeyDown(KeyCode.A))
+      {
+        if (transform.rotation.y >= 170 && transform.rotation.y <= 190)
+        {
+          transform.RotateAround(transform.position, Vector3.up, angle);
+        }
+        
+      }
+      if (Input.GetKeyDown(KeyCode.D))
+      {
+        if (transform.rotation.y == 0)
+        {
+          transform.RotateAround(transform.position, Vector3.up, angle);
+        }
+        
+      }
+      
+      if (Input.GetKeyDown(KeyCode.Space)) 
+      {
+        playerRB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        isOnGround = false;}
+    
+      if (Input.GetKeyDown(KeyCode.F)){
+        if(element == true);
+        {
+          if(facingRight == false)
+          {
+         
+          }
+          if(facingRight == true)
+          {
+          
+          }
+        }
+       if(element == false);
+        {
+          Instantiate(projectileFire, transform.position, projectileFire.transform.rotation);
+        }
+    }
+
+    }
+}
---
+++ NinjaFightGame - Copy/Assets/Script/ProjectileBehavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    public Rigidbody projectileRB;
+    public float speed = 17.0f;
+    public bool facingRight;
+    private PlayerController playerController;
+    public float forwardInput = 15.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        facingRight = playerController.facingRight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(facingRight == true)
+        { 
+        projectileRB.AddForce(transform.forward  * speed);
+        }
+        if(facingRight == false)
+        {
+           transform.Rotate(Vector3.up * 180);
+           projectileRB.AddForce(transform.forward * speed); 
+        }
+    }
+
+
+}
